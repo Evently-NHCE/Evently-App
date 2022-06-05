9f9d2369c6e81d8230cdfdd60e0aa35218b39f2e
Pavel401 <pavelalam401@gmail.com>
2022-06-05 09:25:35 +0530

    initial commit

---
+++ lib/Screens/HomePage/HomePage.dart
@@ -0,0 +1,13 @@
+import 'package:evently/Screens/HomePage/Components/HomePageBody.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: HomePageBody(),
+    );
+  }
+}
