e19c94b54cc3853b1077df89456e14322e23081c
Pavel401 <pavelalam401@gmail.com>
2022-06-05 10:17:17 +0530

    Colour Pallete

---
+++ lib/Utility/Colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+
+import 'package:hexcolor/hexcolor.dart';
+final kBackgroundColor = HexColor("#1E2630");
+final kChipColor = HexColor("#8250CA");
+final kWhiteColor=HexColor("#FFFFFF");
+final ktextWhiteColor=HexColor("#FFFFFF");
+final kTextPurplecolor=HexColor("#B79EE0");
+
+
+//Floating Action Button
+const colorFABbg = Color.fromRGBO(201, 206, 255, 1);
