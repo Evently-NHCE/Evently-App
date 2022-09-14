dd9c7a254a453231bdb1ff47c7eb247cbe0ad6fb
beSaif <hishamkolikara@gmail.com>
2022-09-14 23:07:57 +0530

    Redesigned EventCard

--- lib/Utility/Colors.dart
+++ lib/Utility/Colors.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
-
-
 import 'package:hexcolor/hexcolor.dart';
-final kBackgroundColor = HexColor("#1E2630");
-final kChipColor = HexColor("#8250CA");
-final kWhiteColor=HexColor("#FFFFFF");
-final ktextWhiteColor=HexColor("#FFFFFF");
-final kTextPurplecolor=HexColor("#B79EE0");
 
-final kNavbarcolour=HexColor("#313A4A");
+//final kBackgroundColor = HexColor("#1E2630");
+final kBackgroundColor = HexColor("#303642");
+final kChipColor = HexColor("#8250CA");
+final kWhiteColor = HexColor("#FFFFFF");
+final ktextWhiteColor = HexColor("#FFFFFF");
+final kTextPurplecolor = HexColor("#B79EE0");
+
+final kNavbarcolour = HexColor("#313A4A");
 //Floating Action Button
 const colorFABbg = Color.fromRGBO(201, 206, 255, 1);
