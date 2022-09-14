1734662d1f7ec830c7616706fa38854a633505ab
beSaif <hishamkolikara@gmail.com>
2022-09-14 23:15:53 +0530

    New Tasks: MyEvent and EventPass Screens

---
+++ lib/Screens/EventPass/EventPass.dart
@@ -0,0 +1,16 @@
+import 'package:evently/Screens/EventPass/Components/EventPassBody.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class EventPass extends StatelessWidget {
+  const EventPass({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBody: true,
+      resizeToAvoidBottomInset: false,
+      body: EventPassBody(),
+    );
+  }
+}
