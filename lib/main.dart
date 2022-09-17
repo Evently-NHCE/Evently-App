5575ecca4faac8f39d69861009e4ce786eb5a9da
Pavel401 <pavelalam401@gmail.com>
2022-09-17 22:36:46 +0530

    fix conflicts 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+//import 'package:device_preview_community/device_preview_community.dart';
 import 'package:evently/Screens/DataForm/DataForm.dart';
 import 'package:evently/Screens/HomePage/HomePage.dart';
-import 'package:evently/Screens/My%20Events/MyEvents.dart';
 import 'package:evently/Screens/Navigation/Navigation.dart';
 import 'package:evently/Screens/SignIN/SignIn.dart';
 import 'package:evently/Utility/Colors.dart';
